daa3879f0e90f24ea7f98661831a09657d296867
Alexandre <alexandre@weel.com>
2022-02-03 18:56:50 +0200

    NabBar brought from MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>HI</h1>
+      <NavBar/>
     </div>
   );
 }
